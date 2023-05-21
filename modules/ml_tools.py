1e8226e1ede782bfa7d6784a3e3f52e5d109c6d3
larkz <liu.larkin@gmail.com>
2023-05-21 13:49:01 +0200

    added xgboost pipeline

--- modules/ml_tools.py
+++ modules/ml_tools.py
@@ -3,17 +3,20 @@ import json
 import re
 import copy
 from spacy.lang.en import English
+# from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import xgboost as xgb
 
 nlp = spacy.load("en_core_web_sm")
 
 LEVEL_MAPPING_DIC = {'Internship':0, 'Entry Level':1, 'Mid Level':2, 'Senior Level':3}
 INVERT_LVL_DIC = {value: key for key, value in LEVEL_MAPPING_DIC.items()}
 
-def tokenize_text(text):
-    # Create a Doc object by processing the text with the spaCy model
+def tokenize_text(string_input):
+    text = re.sub(r'[^a-zA-Z0-9\s.,?!]', '', string_input)
     doc = nlp(text)
-    # Extract the tokens from the Doc object and return them as a list
-    tokens = [token.text for token in doc]
+    tokens = [token.lemma_ for token in doc]
     return tokens
 
 def generate_embeddings(data):
@@ -22,9 +25,12 @@ def generate_embeddings(data):
         level = LEVEL_MAPPING_DIC[dic["level"]] if "level" in dic.keys() else None
         title = dic["title"] if "title" in dic.keys() else None
 
-        desc_tokens = tokenize_text(dic["description"])
+        # tokens = tokenize_text(dic["description"].lower())
+        # text_with_sentences = " ".join(tokens).replace(" .", ". ")
+        # doc = nlp(text_with_sentences)  
         doc = nlp(dic["description"])
-        embeddings.append({"level_inf": None, "level": level, "title": title, "desc_text": dic["description"], "doc_vec": doc.vector})
+        
+        embeddings.append({"level_inf": None, "level": level, "title": title, "desc_text": dic["description"], "doc_vec": doc})
     return embeddings
 
 def heuristic_rules(string_input):
@@ -49,10 +55,6 @@ def heuristic_rules(string_input):
     
     return None
 
-
-
-# What is the coverage if we apply heuristic rules?
-
 def infer_lvl_from_rules(embedding_data):
     infer_data = copy.deepcopy(embedding_data)
 
@@ -66,3 +68,23 @@ def infer_lvl_from_rules(embedding_data):
             e["level_inf"] = infered_lvl_desc
     
     return infer_data
+
+def run_xgboost(X_train, X_test, y_train, y_test):
+
+    model = xgb.XGBClassifier(
+        n_estimators=2,  # Increase the number of trees (default: 100)
+        max_depth=20,       # Increase the maximum depth of each tree (default: 3)
+        learning_rate=0.03, # Increase the learning rate (default: 0.1)
+        subsample=0.7,     # Increase the subsample ratio (default: 1.0)
+        colsample_bytree=0.6,  # Increase the feature subsampling ratio (default: 1.0)
+    )
+
+    model.fit(X_train, y_train)
+
+    y_train_pred = model.predict(X_train)
+    accuracy_tr = accuracy_score(y_train, y_train_pred)
+    y_pred = model.predict(X_test)
+    accuracy_test = accuracy_score(y_test, y_pred)
+
+    return model, accuracy_tr, accuracy_test
+    
